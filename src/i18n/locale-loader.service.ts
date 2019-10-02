b09d44ee4982400ffdf7f4e6d6946f188f2532fa
Taylor Buckner <taylora.buckner@gmail.com>
2019-10-02 13:22:15 -0500

    Added node example

--- src/i18n/locale-loader.service.ts
+++ src/i18n/locale-loader.service.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import { resolve, join } from 'path';
 
 export class LocaleLoaderService {
-  private readonly libraryLocaleDirectory = resolve(__dirname, './locales');
+  private readonly libraryLocaleDirectory = resolve(__dirname, './lib/i18n/locales');
 
   public loadFile(filePath: string, userDefined = false): JSON {
     if (!userDefined) {
